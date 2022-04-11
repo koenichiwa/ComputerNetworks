f2e002252aea0c62f3b37ec0bd92e422f58be68c
koenichiwa <47349524+koenichiwa@users.noreply.github.com>
2022-04-11 12:49:39 +0200

    Added mutex to server
    Removed some multithreading bugs

--- assignment_2/server.py
+++ assignment_2/server.py
@@ -1,15 +1,17 @@
-import socket
-import threading
+from socket import socket, AF_INET, SOCK_STREAM
+from threading import Thread, Lock
 
 LOCAL_IP = "localhost"
 LOCAL_PORT = 4242
 
+client_dict_lock = Lock()
 clients = {}
+MAX_CLIENTS = 64
 
 
 def main():
     threads = []
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    with socket(AF_INET, SOCK_STREAM) as sock:
         sock.bind((LOCAL_IP, LOCAL_PORT))
         sock.listen()
         while True:
@@ -19,7 +21,7 @@ def main():
             thread.start()
 
 
-class ClientThread(threading.Thread):
+class ClientThread(Thread):
     def __init__(self, conn, addr):
         super().__init__()
         self.username = None
@@ -37,48 +39,55 @@ class ClientThread(threading.Thread):
             return False
 
         name = data.split()[1]
-        if len(clients) > 64:
-            self.conn.sendall("BUSY\n".encode("utf-8"))
-            return False
+        with client_dict_lock:
+            if len(clients) >= MAX_CLIENTS:
+                self.conn.sendall("BUSY\n".encode("utf-8"))
+                return False
 
-        if name in clients:
-            self.conn.sendall("IN-USE\n".encode("utf-8"))
-            return False
+            if name in clients:
+                self.conn.sendall("IN-USE\n".encode("utf-8"))
+                return False
 
-        self.username = name
-        clients[name] = self.conn
+            self.username = name
+            clients[name] = self.conn
         self.conn.sendall(f"HELLO {name}\n".encode("utf-8"))
-
         return True
+
+    def handle_message(self, data):
+        if data == "WHO\n":
+            with client_dict_lock:
+                client_list = ", ".join(list(clients.keys()))
+            self.conn.sendall(f"WHO-OK {client_list}\n".encode("utf-8"))
+        elif data.startswith("SEND"):
+            split_data = data.split()
+            if len(data < 3):
+                self.conn.sendall("BAD-RQST-BODY\n".encode("utf-8"))
+            else:
+                recipient = split_data[1]
+                message = ' '.join(split_data[2:])
+                if recipient not in clients:
+                    self.conn.sendall(f"UNKNOWN\n".encode("utf-8"))
+                else:
+                    with client_dict_lock:
+                        clients[recipient].sendall(f"DELIVERY {self.username} {message}\n".encode("utf-8"))
+                    self.conn.sendall(f"SEND-OK\n".encode("utf-8"))
+        else:
+            self.conn.sendall("BAD-RQST-HDR\n".encode("utf-8"))
 
     def run(self) -> None:
         while True:
             data = self.conn.recv(2048).decode("utf-8")
             if not data:
                 break
-
             if self.username is None:
                 if not self.handle_new_user(data):
                     break
             else:
-                if data.startswith("SEND"):
-                    split_data = data.split()
-                    recipient = split_data[1]
-                    message = ' '.join(split_data[2:])
-                    if recipient not in clients:
-                        self.conn.sendall(f"UNKNOWN\n".encode("utf-8"))
-                    else:
-                        clients[recipient].sendall(f"DELIVERY {self.username} {message}\n".encode("utf-8"))
-                        self.conn.sendall(f"SEND-OK\n".encode("utf-8"))
-
-                elif data.startswith("WHO"):
-                    client_list = ", ".join(list(clients.keys()))
-                    self.conn.sendall(f"WHO-OK {client_list}\n".encode("utf-8"))
-                else:
-                    self.conn.sendall("BAD-RQST-HDR\n".encode("utf-8"))
-                    break
+                self.handle_message(data)
+                
         if self.username is not None:
-            del clients[self.username]
+            with client_dict_lock:
+                del clients[self.username]
         self.conn.close()
 
 
